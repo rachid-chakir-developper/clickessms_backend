b1d653acf9882a3860ff3573ba198789925dfd46
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2024-05-22 12:09:44 +0200

    add bank accounts

--- finance/models.py
+++ finance/models.py
@@ -5,7 +5,7 @@ import random
 # Create your models here.
 class DecisionDocument(models.Model):
 	number = models.CharField(max_length=255, editable=False, null=True)
-	name = models.CharField(max_length=255)
+	name = models.CharField(max_length=255, null=True)
 	document = models.ForeignKey('medias.File', on_delete=models.SET_NULL, related_name='decision_document_file', null=True)
 	decision_date = models.DateTimeField(null=True)
 	reception_date_time = models.DateTimeField(null=True)
@@ -65,3 +65,60 @@ class DecisionDocumentItem(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+# Create your models here.
+class BankAccount(models.Model):
+	ACCOUNT_TYPES = [
+        ("CURRENT", "Courant"),#
+        ("SAVINGS", "Placement"),#
+        ("FIXED_TERM", "Compte à terme")#
+    ]
+	number = models.CharField(max_length=255, editable=False, null=True)
+	name = models.CharField(max_length=255, null=True)
+	image = models.ForeignKey('medias.File', on_delete=models.SET_NULL, related_name='bank_account_image', null=True)
+	account_number = models.CharField(max_length=255, null=True)
+	account_type = models.CharField(max_length=50, choices=ACCOUNT_TYPES, default= "CURRENT")
+	bank_name = models.CharField(max_length=255, null=True)
+	iban = models.CharField(max_length=255, null=True)
+	bic = models.CharField(max_length=255, null=True)
+	opening_date = models.DateTimeField(null=True)
+	closing_date = models.DateTimeField(null=True)
+	description = models.TextField(default='', null=True)
+	observation = models.TextField(default='', null=True)
+	is_active = models.BooleanField(default=True, null=True)
+	folder = models.ForeignKey('medias.Folder', on_delete=models.SET_NULL, null=True)
+	establishment = models.ForeignKey('companies.establishment', on_delete=models.SET_NULL, related_name='establishment_bank_accounts', null=True)
+	company = models.ForeignKey('companies.Company', on_delete=models.SET_NULL, related_name='company_bank_accounts', null=True)
+	creator = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True)
+	is_deleted = models.BooleanField(default=False, null=True)
+	created_at = models.DateTimeField(auto_now_add=True, null=True)
+	updated_at = models.DateTimeField(auto_now=True, null=True)
+    
+	def save(self, *args, **kwargs):
+	    # Générer le numéro unique lors de la sauvegarde si ce n'est pas déjà défini
+	    if not self.number:
+	        self.number = self.generate_unique_number()
+
+	    super(BankAccount, self).save(*args, **kwargs)
+
+	def generate_unique_number(self):
+	    # Implémentez la logique de génération du numéro unique ici
+	    # Vous pouvez utiliser des combinaisons de date, heure, etc.
+	    # par exemple, en utilisant la fonction strftime de l'objet datetime
+	    # pour générer une chaîne basée sur la date et l'heure actuelles.
+
+	    # Exemple : Utilisation de la date et de l'heure actuelles
+	    current_time = datetime.now()
+	    number_suffix = current_time.strftime("%Y%m%d%H%M%S")
+	    number_prefix = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=2))  # Ajoutez 3 lettres au début
+	    number = f'{number_prefix}{number_suffix}'
+
+	    # Vérifier s'il est unique dans la base de données
+	    while BankAccount.objects.filter(number=number).exists():
+	        number_suffix = current_time.strftime("%Y%m%d%H%M%S")
+	        number = f'{number_prefix}{number_suffix}'
+
+	    return number
+	    
+	def __str__(self):
+		return self.name
--- finance/schema.py
+++ finance/schema.py
@@ -6,7 +6,7 @@ from graphene_file_upload.scalars import Upload
 
 from django.db.models import Q
 
-from finance.models import DecisionDocument, DecisionDocumentItem
+from finance.models import DecisionDocument, DecisionDocumentItem, BankAccount
 from medias.models import Folder, File
 
 class DecisionDocumentItemType(DjangoObjectType):
@@ -30,6 +30,23 @@ class DecisionDocumentNodeType(graphene.ObjectType):
     total_count = graphene.Int()
 
 class DecisionDocumentFilterInput(graphene.InputObjectType):
+    keyword = graphene.String(required=False)
+    starting_date_time = graphene.DateTime(required=False)
+    ending_date_time = graphene.DateTime(required=False)
+
+class BankAccountType(DjangoObjectType):
+    class Meta:
+        model = BankAccount
+        fields = "__all__"
+    image = graphene.String()
+    def resolve_image( instance, info, **kwargs ):
+        return instance.image and info.context.build_absolute_uri(instance.image.image.url)
+
+class BankAccountNodeType(graphene.ObjectType):
+    nodes = graphene.List(BankAccountType)
+    total_count = graphene.Int()
+
+class BankAccountFilterInput(graphene.InputObjectType):
     keyword = graphene.String(required=False)
     starting_date_time = graphene.DateTime(required=False)
     ending_date_time = graphene.DateTime(required=False)
@@ -61,9 +78,27 @@ class DecisionDocumentInput(graphene.InputObjectType):
     financier_id = graphene.Int(name="financier", required=False)
     decision_document_items = graphene.List(DecisionDocumentItemInput, required=False)
 
+class BankAccountInput(graphene.InputObjectType):
+    id = graphene.ID(required=False)
+    number = graphene.String(required=False)
+    name = graphene.String(required=False)
+    account_number = graphene.String(required=False)
+    account_type = graphene.String(required=False)
+    bank_name = graphene.String(required=False)
+    iban = graphene.String(required=False)
+    bic = graphene.String(required=False)
+    opening_date = graphene.DateTime(required=False)
+    closing_date = graphene.DateTime(required=False)
+    description = graphene.String(required=False)
+    observation = graphene.String(required=False)
+    is_active = graphene.Boolean(required=False)
+    establishment_id = graphene.Int(name="establishment", required=False)
+
 class FinanceQuery(graphene.ObjectType):
     decision_documents = graphene.Field(DecisionDocumentNodeType, decision_document_filter= DecisionDocumentFilterInput(required=False), offset = graphene.Int(required=False), limit = graphene.Int(required=False), page = graphene.Int(required=False))
     decision_document = graphene.Field(DecisionDocumentType, id = graphene.ID())
+    bank_accounts = graphene.Field(BankAccountNodeType, bank_account_filter= BankAccountFilterInput(required=False), offset = graphene.Int(required=False), limit = graphene.Int(required=False), page = graphene.Int(required=False))
+    bank_account = graphene.Field(BankAccountType, id = graphene.ID())
     def resolve_decision_documents(root, info, decision_document_filter=None, offset=None, limit=None, page=None):
         # We can easily optimize query count in the resolve method
         user = info.context.user
@@ -95,6 +130,38 @@ class FinanceQuery(graphene.ObjectType):
         except DecisionDocument.DoesNotExist:
             decision_document = None
         return decision_document
+
+    def resolve_bank_accounts(root, info, bank_account_filter=None, offset=None, limit=None, page=None):
+        # We can easily optimize query count in the resolve method
+        user = info.context.user
+        company = user.current_company if user.current_company is not None else user.company
+        total_count = 0
+        bank_accounts = BankAccount.objects.filter(company=company)
+        if bank_account_filter:
+            keyword = bank_account_filter.get('keyword', '')
+            starting_date_time = bank_account_filter.get('starting_date_time')
+            ending_date_time = bank_account_filter.get('ending_date_time')
+            if keyword:
+                bank_accounts = bank_accounts.filter(Q(name__icontains=keyword) | Q(registration_number__icontains=keyword) | Q(driver_name__icontains=keyword))
+            if starting_date_time:
+                bank_accounts = bank_accounts.filter(created_at__gte=starting_date_time)
+            if ending_date_time:
+                bank_accounts = bank_accounts.filter(created_at__lte=ending_date_time)
+        bank_accounts = bank_accounts.order_by('-created_at')
+        total_count = bank_accounts.count()
+        if page:
+            offset = limit * (page - 1)
+        if offset is not None and limit is not None:
+            bank_accounts = bank_accounts[offset:offset + limit]
+        return BankAccountNodeType(nodes=bank_accounts, total_count=total_count)
+
+    def resolve_bank_account(root, info, id):
+        # We can easily optimize query count in the resolve method
+        try:
+            bank_account = BankAccount.objects.get(pk=id)
+        except BankAccount.DoesNotExist:
+            bank_account = None
+        return bank_account
 
 #************************************************************************
 
@@ -225,8 +292,128 @@ class DeleteDecisionDocument(graphene.Mutation):
         return DeleteDecisionDocument(deleted=deleted, success=success, message=message, id=id)
 
 #*************************************************************************#
+
+#************************************************************************
+
+class CreateBankAccount(graphene.Mutation):
+    class Arguments:
+        bank_account_data = BankAccountInput(required=True)
+        image = Upload(required=False)
+
+    bank_account = graphene.Field(BankAccountType)
+
+    def mutate(root, info, image=None, bank_account_data=None):
+        creator = info.context.user
+        bank_account = BankAccount(**bank_account_data)
+        bank_account.creator = creator
+        bank_account.company = creator.current_company if creator.current_company is not None else creator.company
+        if info.context.FILES:
+            # file1 = info.context.FILES['1']
+            if image and isinstance(image, UploadedFile):
+                image_file = bank_account.image
+                if not image_file:
+                    image_file = File()
+                    image_file.creator = creator
+                image_file.image = image
+                image_file.save()
+                bank_account.image = image_file
+        bank_account.save()
+        folder = Folder.objects.create(name=str(bank_account.id)+'_'+bank_account.name,creator=creator)
+        bank_account.folder = folder
+        bank_account.save()
+        return CreateBankAccount(bank_account=bank_account)
+
+class UpdateBankAccount(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        bank_account_data = BankAccountInput(required=True)
+        image = Upload(required=False)
+
+    bank_account = graphene.Field(BankAccountType)
+
+    def mutate(root, info, id, image=None, bank_account_data=None):
+        creator = info.context.user
+        BankAccount.objects.filter(pk=id).update(**bank_account_data)
+        bank_account = BankAccount.objects.get(pk=id)
+        if not bank_account.folder or bank_account.folder is None:
+            folder = Folder.objects.create(name=str(bank_account.id)+'_'+bank_account.name,creator=creator)
+            BankAccount.objects.filter(pk=id).update(folder=folder)
+        if not image and bank_account.image:
+            image_file = bank_account.image
+            image_file.delete()
+        if info.context.FILES:
+            # file1 = info.context.FILES['1']
+            if image and isinstance(image, UploadedFile):
+                image_file = bank_account.image
+                if not image_file:
+                    image_file = File()
+                    image_file.creator = creator
+                image_file.image = image
+                image_file.save()
+                bank_account.image = image_file
+            bank_account.save()
+        return UpdateBankAccount(bank_account=bank_account)
+
+class UpdateBankAccountState(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    bank_account = graphene.Field(BankAccountType)
+    done = graphene.Boolean()
+    success = graphene.Boolean()
+    message = graphene.String()
+
+    def mutate(root, info, id, bank_account_fields=None):
+        creator = info.context.user
+        done = True
+        success = True
+        bank_account = None
+        message = ''
+        try:
+            bank_account = BankAccount.objects.get(pk=id)
+            BankAccount.objects.filter(pk=id).update(is_active=not bank_account.is_active)
+            bank_account.refresh_from_db()
+        except Exception as e:
+            done = False
+            success = False
+            bank_account=None
+            message = "Une erreur s'est produite."
+        return UpdateBankAccountState(done=done, success=success, message=message,bank_account=bank_account)
+
+
+class DeleteBankAccount(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    bank_account = graphene.Field(BankAccountType)
+    id = graphene.ID()
+    deleted = graphene.Boolean()
+    success = graphene.Boolean()
+    message = graphene.String()
+
+    def mutate(root, info, id):
+        deleted = False
+        success = False
+        message = ''
+        current_user = info.context.user
+        if current_user.is_superuser:
+            bank_account = BankAccount.objects.get(pk=id)
+            bank_account.delete()
+            deleted = True
+            success = True
+        else:
+            message = "Vous n'êtes pas un Superuser."
+        return DeleteBankAccount(deleted=deleted, success=success, message=message, id=id)
+
+#*************************************************************************#
+
 class FinanceMutation(graphene.ObjectType):
     create_decision_document = CreateDecisionDocument.Field()
     update_decision_document = UpdateDecisionDocument.Field()
     update_decision_document_state = UpdateDecisionDocumentState.Field()
     delete_decision_document = DeleteDecisionDocument.Field()
+
+    create_bank_account = CreateBankAccount.Field()
+    update_bank_account = UpdateBankAccount.Field()
+    update_bank_account_state = UpdateBankAccountState.Field()
+    delete_bank_account = DeleteBankAccount.Field()
