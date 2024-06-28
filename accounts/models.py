f770b5509ed103af4daee013eb7c299f26b0449d
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2024-06-28 15:17:44 +0200

    edit roles choices

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Role(models.Model):
     ROLE_CHOICES = [
+        ('SUPER_ADMIN', 'Super admin'),
         ('ADMIN', 'Admin'),
         ('MANAGER', 'Employé'),
         ('QUALITY_MANAGER', 'Responsable Qualité'),
