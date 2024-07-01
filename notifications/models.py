ac2b51a69c50c1e40b24074d9af7116dec8b0c7b
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2024-07-01 09:42:51 +0200

    put welcome messages and noyifications

--- notifications/models.py
+++ notifications/models.py
@@ -55,6 +55,7 @@ class MessageNotification(models.Model):
 	message = models.TextField(null=True)
 	is_active = models.BooleanField(default=True, null=True)
 	company = models.ForeignKey('companies.Company', on_delete=models.SET_NULL, related_name='message_notifications', null=True)
+	creator = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, related_name='message_notifications', null=True)
 	created_at = models.DateTimeField(auto_now_add=True, null=True)
 	updated_at = models.DateTimeField(auto_now=True, null=True)
 
