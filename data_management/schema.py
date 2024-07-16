b2190aa05c196b852866a2c77af2bfe7c15fb6be
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2024-07-16 09:56:19 +0200

    update registration_number on import

--- data_management/schema.py
+++ data_management/schema.py
@@ -204,6 +204,8 @@ def import_data_from_file(model, file, fields, user=None):
             employee = model.objects.get(Q(registration_number=registration_number) | Q(social_security_number=social_security_number) | Q(first_name=first_name, last_name=last_name))
             if not employee.social_security_number or employee.social_security_number == '' :
                 model.objects.filter(pk=employee.id).update(social_security_number=social_security_number)
+            if not employee.registration_number or employee.registration_number == '' :
+                model.objects.filter(pk=employee.id).update(registration_number=registration_number)
         except model.DoesNotExist:
             model.objects.create(
                 registration_number=registration_number,
