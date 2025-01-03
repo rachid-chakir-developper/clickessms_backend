38b719bf1ee55d670d37b7aba062f737104e4ee2
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2025-01-03 10:12:23 +0100

    table activity presence count fix beug date -2

--- human_ressources/models.py
+++ human_ressources/models.py
@@ -551,6 +551,9 @@ class BeneficiaryEntry(models.Model):
         start_of_year = datetime(year, 1, 1)
         end_of_year = datetime(year, 12, 31)
 
+        print(f"start_of_year : {start_date} => {type(start_date)}")
+        print(f"end_of_year : {end_date} => {type(end_date)}")
+
         # Si les dates sont naïves, les rendre conscientes
         start_of_year = make_aware(start_of_year) if start_of_year.tzinfo is None else start_of_year
         end_of_year = make_aware(end_of_year) if end_of_year.tzinfo is None else end_of_year
