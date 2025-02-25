0a15242b5c616a6c36f6469a7fa3036c59009a56
Rachid CHAKIR <rachid.chakir.developper@gmail.com>
2025-02-25 12:31:41 +0100

    fix beug calc available places and month

--- dashboard/schema.py
+++ dashboard/schema.py
@@ -467,7 +467,7 @@ class DashboardActivityType(graphene.ObjectType):
         # Obtenir l'année en cours pour filtrer par année
         date = datetime.date.today()
         year=str(date.year)
-        month=date.month
+        month=date.month - 1
         start_year = date.replace(month=1, day=1)  # Début de l'année
         end_year = date.replace(month=12, day=31)  # Fin de l'année
 
