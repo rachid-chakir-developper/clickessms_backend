30f3e0ac809d0150e779506da579f9059287ff3a
Samuel761200 <samuel.saunier761200@gmail.com>
2025-03-27 17:09:51 +0100

    Module acompte

--- human_ressources/models.py
+++ human_ressources/models.py
@@ -1161,7 +1161,7 @@ class CareerEntry(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
-        ordering = ['starting_date']
+        ordering = ['starting_date'] 
 
 class Advance(models.Model):
     """
@@ -1175,7 +1175,7 @@ class Advance(models.Model):
     ]
     
     number = models.CharField(max_length=255, editable=False, null=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     month = models.DateField(help_text="Mois pour lequel l'acompte est demandé")
     reason = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
